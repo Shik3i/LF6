b100c3b6e96b9fc76cbf21f2ddf4d3ad43e67ec5
Shik3i <6156589+Shik3i@users.noreply.github.com>
2019-10-22 08:43:55 +0200

    Create sketch_SOS_Morse.ino

---
+++ ESP32/ino Dateien/Tests/sketch_SOS_Morse/sketch_SOS_Morse.ino
@@ -0,0 +1,36 @@
+void setup() {
+  // put your setup code here, to run once:
+  
+  pinMode(22, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly
+  shortBeep();
+  shortBeep();
+  shortBeep();
+  longBeep();
+  longBeep();
+  longBeep();
+  shortBeep();
+  shortBeep();
+  shortBeep();
+  delay(1000);
+}
+
+
+
+void shortBeep(void) {
+  digitalWrite(22, HIGH);
+  delay(250);
+  digitalWrite(22, LOW);
+  delay(250);
+}
+
+void longBeep(void) {
+  digitalWrite(22, HIGH);
+  delay(500);
+  digitalWrite(22, LOW);
+  delay(500);
+}
