215a2ef71d1df99f01c82b47641ada447dc51626
Shik3i <6156589+Shik3i@users.noreply.github.com>
2019-12-11 10:57:43 +0100

    bubblesort hinzugefügt

--- Aufgabe5/Median.java
+++ Aufgabe5/Median.java
@@ -14,7 +14,24 @@ class Median{
         }
         scanner.close();
 
+        // Sortieren mit bubblesort, zwar langsam(O(n²)) aber am einfachsten/schnellsten zu implementieren/erklären
+        numbers = bubblesort(numbers);
+
         // für jede ungerade Anzahl an Zahlen "n" ist der Median immer bei n / 2
         System.out.println("Median: " + numbers[n / 2]);
+    }
+
+    private static int[] bubblesort(int[] numbers) {
+        for (int i = 1; i < numbers.length; i++) {
+			for (int j = 0; j < numbers.length - i; j++) {
+				if (numbers[j] > numbers[j+1]) {
+					int temp = numbers[j];
+					numbers[j] = numbers[j+1];
+					numbers[j+1] = temp;
+				}
+				
+			}
+		}
+        return numbers;
     }
 }
